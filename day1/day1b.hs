6b2bf1664bc7ca626d83eac32402dfd668625808
Jean-Pierre Laurin <jplaurin@gmail.com>
2018-12-06 23:01:04 -0500

    optimiser day1b avec IntSet au lieu d'elem de list

--- day1/day1b.hs
+++ day1/day1b.hs
@@ -1,9 +1,13 @@
 --- file: day1b.hs
+import Data.IntSet (IntSet)
+import qualified Data.IntSet as IntSet
 
-main = interact ((++"\n").show.findPremierDup [0].cycle.map (\s -> read s ::Integer).lines.(filter (/='+')))
+main = interact ((++"\n").show.findPremierDup.cycle.map (\s -> read s ::Int).lines.(filter (/='+')))
 
-findPremierDup xs (y:ys) 
-    | n `elem` xs = n
-    | otherwise = findPremierDup (n:xs) ys
-      where n = y + head xs
-findPremierDup _ _ = 0 
+findPremierDup xs = findLoop 0 (IntSet.fromList [0]) xs
+
+findLoop acc s (y:ys) 
+    | IntSet.member n s = n
+    | otherwise = {-# SCC "chercheDup" #-} findLoop n (IntSet.insert n s) ys
+      where n = y + acc
+findLoop _ _ _ = 0 
