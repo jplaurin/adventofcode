dfc0abf4d72f3450c6dbd420057a686314ee03a4
Jean-Pierre Laurin <jplaurin@gmail.com>
2018-12-08 16:39:27 -0500

    changed to IntSet to speed up

--- day3/day3.hs
+++ day3/day3.hs
@@ -1,36 +1,39 @@
 -- file: day3.hs
 import Data.List
-import Data.Set (Set)
-import qualified Data.Set as Set
+import Data.IntSet (IntSet)
+import qualified Data.IntSet as IntSet
 
 main = interact day3
 
 day3 = affiche.process.obtient
 
 process xs = [a,b]
-    where a             = Set.size listOfOverlap
+    where a             = IntSet.size listOfOverlap
           b             = findNoOverlap xs listOfOverlap
-          listOfOverlap = (Set.unions).getListIntersect $ xs
+          listOfOverlap = (IntSet.unions).getListIntersect $ xs
 
 findNoOverlap xs overlapSet = head [ noId | x<-xs, 
                                 let noId = head x, 
                                 let sa = getPosSet x,
-                                let interSet = (Set.intersection ) sa overlapSet,
-                                null interSet]
+                                let interSet = (IntSet.intersection ) sa overlapSet,
+                                (IntSet.null) interSet]
 
 getListIntersect (x:xs) = getListLoop x xs
 
 getListLoop _ [] = []
-getListLoop x xs = nub ( [ns| sb<-ls, let ns=Set.intersection sa sb, not(null ns) ] ++
-                         (getListLoop (head xs) (tail xs)) )
+getListLoop x xs = nub ( [ns| sb<-ls, 
+                           let ns=IntSet.intersection sa sb, 
+                           not((IntSet.null) ns) ] 
+                         ++ (getListLoop (head xs) (tail xs)) )
                 where  sa = getPosSet x;
                        ls = map getPosSet xs
 
-getPosSet :: [Int] -> Set (Int,Int)
-getPosSet [_,x0,y0,w,h] = Set.fromList listOfPos
-    where listOfPos = [(x,y)| x<-[x0..(x0+w-1)], 
-                              y<-[y0..(y0+h-1)] ]
-getPosSet _ = Set.fromList []
+getPosSet :: [Int] -> IntSet
+getPosSet [_,x0,y0,w,h] = IntSet.fromList listOfPos
+    where listOfPos = [pos | x<-[x0..(x0+w-1)], 
+                              y<-[y0..(y0+h-1)],
+                              let pos = 1000*x + y]
+getPosSet _ = IntSet.fromList []
 
 obtient = map (lineToFiveInt).lines
 
