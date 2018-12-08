a065fbcc18586e19a8768107d18947bb87a6a01f
Jean-Pierre Laurin <jplaurin@gmail.com>
2018-12-08 14:57:31 -0500

    day3 part A working

--- day3/day3.hs
+++ day3/day3.hs
@@ -1,11 +1,27 @@
 -- file: day3.hs
--- import Data.Set
+import Data.List
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 main = interact day3
 
 day3 = affiche.process.obtient
 
-process = id
+process xs = (Set.size).(Set.unions).getListIntersect $ xs
+
+getListIntersect (x:xs) = getListLoop x xs
+
+getListLoop _ [] = []
+getListLoop x xs = nub ( [ns| sb<-ls, let ns=Set.intersection sa sb, not(null ns) ] ++
+                         (getListLoop (head xs) (tail xs)) )
+                where  sa = getPosSet x;
+                       ls = map getPosSet xs
+
+getPosSet :: [Int] -> Set (Int,Int)
+getPosSet [x0,y0,w,h] = Set.fromList listOfPos
+    where listOfPos = [(x,y)| x<-[x0..(x0+w-1)], 
+                              y<-[y0..(y0+h-1)] ]
+getPosSet _ = Set.fromList []
 
 obtient = map (lineToFourInt).lines.filter (/=':')
 
@@ -17,4 +33,4 @@ putSpace c = c
   
 sToI s = read s :: Int
  
-affiche = unlines.map show
+affiche x = unlines [show x]
