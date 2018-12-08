c094c8de0a8a1541cba66079c61b4a706b478fc0
Jean-Pierre Laurin <jplaurin@gmail.com>
2018-12-08 00:27:15 -0500

    added part2

--- day2/day2.hs
+++ day2/day2.hs
@@ -3,7 +3,11 @@ import Data.List
 
 main = interact day2
 
-day2 = presente.calculeChecksum.obtient
+day2 = presente.process.obtient
+
+process xs = (a,b)
+    where a = calculeChecksum xs
+          b = tryAllPair xs
 
 calculeChecksum xs = a * b
     where a = sum.map fst $ listOccurence
@@ -15,6 +19,11 @@ compte xs= (a,b)
           b = length.filter (==3) $ freqList
           freqList = nub.map length.group.sort $ xs
 
+tryAllPair xs = nub [common | x<-xs,y<-xs,x/=y, let common = findCommon x y, let l = length common , l==(ml-1) ]
+    where ml = maximum.map length $ xs
+
+findCommon xs ys = map fst.filter (\(a,b)-> a==b ).zip xs $ ys
+
 obtient = lines
 
-presente = (++"\n").show
+presente (a,b) = (show a) ++"\n" ++ (head b) ++ "\n"
