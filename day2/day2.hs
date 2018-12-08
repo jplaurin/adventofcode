1a827031aad3e213eda3e4c24a9daf4bbfb214b6
Jean-Pierre Laurin <jplaurin@gmail.com>
2018-12-07 23:37:55 -0500

    add day2 part A

---
+++ day2/day2.hs
@@ -0,0 +1,21 @@
+-- file: day2.hs
+import Data.List
+
+main = interact day2
+
+--day2 = presente.calculeChecksum.obtient
+day2 = presente.calculeChecksum.obtient
+
+calculeChecksum xs = a * b
+    where a = sum.map fst $ listOccurence
+          b = sum.map snd $ listOccurence
+          listOccurence = map compte $ xs
+
+compte xs= (a,b)
+    where a = length.filter (==2) $ freqList
+          b = length.filter (==3) $ freqList
+          freqList = nub.map length.group.sort $ xs
+
+obtient = lines
+
+presente = (++"\n").show
