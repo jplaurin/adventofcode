0efb48498cff12ad2852485c860a3e2c17937dd9
Jean-Pierre Laurin <jplaurin@gmail.com>
2018-12-08 17:45:33 -0500

    optimiser et reduit le temps de moitier mais encore trop long a 16sec

--- day3/day3.hs
+++ day3/day3.hs
@@ -18,21 +18,23 @@ findNoOverlap xs overlapSet = head [ noId | x<-xs,
                                 let interSet = (IntSet.intersection ) sa overlapSet,
                                 (IntSet.null) interSet]
 
-getListIntersect (x:xs) = getListLoop x xs
+getListIntersect (x:xs) = getListLoop x xs []
 
-getListLoop _ [] = []
-getListLoop x xs = nub ( [ns| sb<-ls, 
-                           let ns=IntSet.intersection sa sb, 
-                           not((IntSet.null) ns) ] 
-                         ++ (getListLoop (head xs) (tail xs)) )
+getListLoop _ [] res = res
+getListLoop x xs res = getListLoop (head xs) (tail xs) newRes    -- slow
                 where  sa = getPosSet x;
-                       ls = map getPosSet xs
+                       ls = map getPosSet xs                      -- slow
+                       newRes = (res ++ curRes)
+                       curRes = [ns | sb<-ls, 
+                                      let ns=IntSet.intersection sa sb, 
+                                      not((IntSet.null) ns) ]
 
 getPosSet :: [Int] -> IntSet
-getPosSet [_,x0,y0,w,h] = IntSet.fromList listOfPos
-    where listOfPos = [pos | x<-[x0..(x0+w-1)], 
+getPosSet [_,x0,y0,w,h] = posSet
+    where posSet    = IntSet.fromAscList listOfPos
+          listOfPos = [pos | x<-[x0..(x0+w-1)],                   -- slow
                               y<-[y0..(y0+h-1)],
-                              let pos = 1000*x + y]
+                              let pos = 1024*x + y]
 getPosSet _ = IntSet.fromList []
 
 obtient = map (lineToFiveInt).lines
