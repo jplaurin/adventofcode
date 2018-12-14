f8cf30dec53b9ccb87817440959c0efaa2e5eacf
root <root@DESKTOP-UKOHQKU.localdomain>
2018-12-14 00:07:48 -0500

    just reading data to a list of record for now

---
+++ day4/day4.hs
@@ -0,0 +1,23 @@
+-- file: day4.hs
+import Data.Char
+import Data.List
+
+main = interact day4
+
+day4 = affiche.process.obtient
+
+process = id
+
+affiche = unlines.map show
+obtient = map lineTo5Value .sort.lines
+lineTo5Value = wordsTo5Values.words.map punctuationToSpace
+
+punctuationToSpace x 
+    | not.isAlphaNum $ x = ' '
+    | otherwise = x
+    
+wordsTo5Values (y:m:d:h:mi:t:g:_)
+    | t == "Guard" = (y,m,d,h,mi,read g,0)
+    | t == "falls" = (y,m,d,h,mi,0,1)
+    | t == "wakes" = (y,m,d,h,mi,0,0)
+
